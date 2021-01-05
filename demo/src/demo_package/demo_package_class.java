f76ac4c9d5e8ac13d7ff54eb1be26b766f37b5c9
murat <murat@DESKTOP-MCLVAPU>
2021-01-05 22:21:05 +0300

    test_deneme commit

---
+++ demo/src/demo_package/demo_package_class.java
@@ -0,0 +1,32 @@
+package demo_package;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo_package_class {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+//setting the driver executable
+System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+//Initiating your chromedriver
+WebDriver driver=new ChromeDriver();
+
+//Applied wait time
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//maximize window
+driver.manage().window().maximize();
+
+//open browser with desried URL
+driver.get("https://www.google.com");
+
+//closing the browser
+driver.close();
+
+}
+
+}
